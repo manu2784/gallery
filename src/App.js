01c3960623372730efb79636a189a26131bc7a29
Mallesh Ashnikar <mallesh.ashnikar@ecoonline.com>
2020-02-07 18:18:07 +0000

    added grid compnent from material ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import ImageGrid from "./components/grid/ImageGrid";
-import {
-  Container,
-  AppBar,
-  Toolbar
-} from '@material-ui/core';
+import { Container, AppBar, Toolbar } from "@material-ui/core";
 
 function App() {
   return (
-<Container fixed>
-<AppBar position="fixed">
-<Container fixed>
-    <Toolbar>{/* content */}</Toolbar>
-  </Container>
-  </AppBar>   
- <ImageGrid/>
-  
-</Container>
-    
+    <Container fixed>
+      <AppBar position="fixed">
+        <Container fixed>
+          <Toolbar>{/* content */}</Toolbar>
+        </Container>
+      </AppBar>
+      <ImageGrid />
+    </Container>
   );
 }
 
--- src/components/grid/ImageGrid.js
+++ src/components/grid/ImageGrid.js
@@ -1,28 +1,85 @@
 import React from "react";
+import { withStyles } from '@material-ui/styles';
+import {
+    GridList,
+    GridListTile,
+    TablePagination
+} from "@material-ui/core/";
 
-export default class ImageGrid extends React.Component {
+
+
+const styles = theme => {
+    return ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: "#fff",
+    },
+
+  })}
+
+class ImageGrid extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
         images: null,
         loading:true,
+        page:1,
+        imagesPerPage:12
     }
   }
 
-   componentDidMount(){
-    fetch("https://cors-anywhere.herokuapp.com/https://pixabay.com/api/?key=6473511-0417f2cad683f1bee54cafe15")
+  handleChangePage = (event, newPage)=>{
+      this.setState({page:newPage});
+      this.getImages();
+  }
+
+  handleChangeRowsPerPage= (event)=>{
+        this.setState({imagesPerPage: event.target.value});
+        this.getImages();
+  }
+  
+  getImages(){
+          // get images 
+    fetch("https://cors-anywhere.herokuapp.com/https://pixabay.com/api/?key=6473511-0417f2cad683f1bee54cafe15&page="+this.state.page+"&per_page="+this.state.imagesPerPage)
     .then(res=>res.json())
     .then(body=>{
-        this.setState({images:body.hits, loading:false});
+        this.setState({images:body.hits, loading:false, imageCount:body.totalHits});
     });
+  }
+   componentDidMount(){
+       this.getImages();
    
   }
 
   render(){
-    console.log(this.state.loading);
+      if(this.state.images)
+    console.log(this.state.images[0]);
+    const { classes } = this.props;
     return (
-     <h1>Hello</h1>
+        <div className={classes.root}>
+        <GridList cellHeight={160} className={classes.gridList} cols={3}>
+        {this.state.images && this.state.images.map(img => (
+          <GridListTile key={img.id} cols={1}>
+            <img src={img.largeImageURL} alt={img.id} />
+          </GridListTile>
+        ))}
+            <TablePagination
+                 rowsPerPageOptions={[6, 12, 24]}
+                 component="div"
+                 count={this.state.images? this.state.imageCount : 0}
+                 rowsPerPage={this.state.imagesPerPage}
+                 page={this.state.page}
+                 onChangePage={this.handleChangePage}
+                 onChangeRowsPerPage={this.handleChangeRowsPerPage}
+               />
+      </GridList>
+      </div>
     )
   }
 }
+
+export default withStyles(styles, { withTheme: true })(ImageGrid);
